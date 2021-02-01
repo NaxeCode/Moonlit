97f01af79bde4e5b7a6637bd0e1ee47d98d0e6ec
Seraj Lee <Lucas_Naruto_AE@Yahoo.com>
2021-02-01 15:46:51 -0800

    Messing around to get familiar w/ Gatsby's workflow in comparison to React

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: "purple" }}>
+      <h1>After Dark Header :)</h1>
+      <p>Can't wait for this site to start running properly.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  )
 }

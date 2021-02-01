176c45223a5b3802b8fa0b06e0f02f43b4b75f6d
Seraj Lee <Lucas_Naruto_AE@Yahoo.com>
2021-02-01 15:56:20 -0800

    Edited Header wording

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Home() {
   return (
     <div style={{ color: "purple" }}>
-      <h1>After Dark Header :)</h1>
+      <h1>After Dark :)</h1>
       <p>Can't wait for this site to start running properly.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>

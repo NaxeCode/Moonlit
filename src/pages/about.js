a02bebd68f4f1ad1dd816b5cd61d01bff8a53cba
Seraj Lee <Lucas_Naruto_AE@Yahoo.com>
2021-02-01 15:56:27 -0800

    Added an about page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div style={{ color: "teal" }}>
+      <h1>More about the Moonlit Project...</h1>
+      <p>
+        An 18+ site prioritizing quality and ease-of-use for industry
+        professionals with a focus on good profit-margins for creators
+      </p>
+    </div>
+  )
+}

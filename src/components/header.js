119d0fee8af9934d6196adad5e8c6b89d24c3a76
Seraj Lee <Lucas_Naruto_AE@Yahoo.com>
2021-02-01 16:37:26 -0800

    Edited Header component and implemented into main web page

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import React from "react"
 
-export default function Header() {
-  return <h1>This is an example Header :)</h1>
+export default function Header(props) {
+  return <h1>{props.headerText}</h1>
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from "react"
+import Header from "../components/header"
 
 export default function Home() {
   return (
     <div style={{ color: "purple" }}>
-      <h1>After Dark :)</h1>
+      <Header headerText="After Dark :)" />
       <p>Can't wait for this site to start running properly.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>

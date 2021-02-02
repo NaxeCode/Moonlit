212994417aac9565fc164a134008330d8b84fa82
Seraj Lee <Lucas_Naruto_AE@Yahoo.com>
2021-02-01 16:44:43 -0800

    Added contact us link onto homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 import Header from "../components/header"
 
 export default function Home() {
   return (
     <div style={{ color: "purple" }}>
       <Header headerText="After Dark :)" />
+      <Link to="/contact/">Contact</Link>
       <p>Can't wait for this site to start running properly.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>

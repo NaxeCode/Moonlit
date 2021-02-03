6e40b187abd567514c40a4f93b5e3531cf652984
Seraj Lee <Lucas_Naruto_AE@Yahoo.com>
2021-02-03 09:33:44 -0800

    adjusted about page

---
+++ src/pages/aboutcss.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Container from "../components/container"
+
+export default function About() {
+  return (
+    <Container>
+      <h1>CSS CONTAINER</h1>
+      <p>CSS MODULES ARE GREAT</p>
+    </Container>
+  )
+}

3d2cf283d730e095f31a84064d742d9680034630
Seraj Lee <Lucas_Naruto_AE@Yahoo.com>
2021-02-03 09:33:06 -0800

    Implemened Firebase API

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,11 +3,18 @@ import React from "react"
 export default function About() {
   return (
     <div style={{ color: "teal" }}>
-      <h1>More about the Moonlit Project...</h1>
-      <p>
-        An 18+ site prioritizing quality and ease-of-use for industry
-        professionals with a focus on good profit-margins for creators
-      </p>
+      <body>
+        <script src="/__/firebase/8.2.5/firebase-app.js"></script>
+
+        <script src="/__/firebase/8.2.5/firebase-analytics.js"></script>
+
+        <script src="/__/firebase/init.js"></script>
+        <h1>More about the Moonlit Project...</h1>
+        <p>
+          An 18+ site prioritizing quality and ease-of-use for industry
+          professionals with a focus on good profit-margins for creators
+        </p>
+      </body>
     </div>
   )
 }
